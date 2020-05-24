a03c55c673df78f7fa72be09a6efa1d9e6d028b0
Robert Barnes <robertbarnes@googlemail.com>
2020-05-24 10:17:01 +0100

    Refactor to execute tasks at same time

--- speeduino_can_bridge.ino
+++ speeduino_can_bridge.ino
@@ -15,25 +15,24 @@
 #ifdef DEBUG
 #include <stdio.h>
 char debugBuffer[255];
+#define DEBUG_BAUD 115200
 #endif
 
 #ifdef USE_SW_SERIAL
 #include <SoftwareSerial.h>
+#define SW_SERIAL_RX 7
+#define SW_SERIAL_TX 8
 #endif
 
 #define SPEEDUINO_CANID 0x5A1
-#define SPEEDUINO_R_COMMAND 0x30
-#define SERIAL_BAUD 115200
+#define SPEEDUINO_COMMAND 'r'
+#define SPEEDUINO_COMMAND_TYPE 0x30
 #define SPEEDUINO_BAUD 115200
-
-#define MILLIS_BETWEEN_READS 100
 
 #define DATA_TO_REQUEST 16
 #define COOLANT_OFFSET 7
-#define RPM_OFFSET 14
-
-#define SW_SERIAL_RX 7
-#define SW_SERIAL_TX 8
+#define RPM_OFFSET_LO 14
+#define RPM_OFFSET_HI 15
 
 #define MCP2515_CS SPI_CS
 #define MCP2515_BITRATE CAN_500KBPS
@@ -42,29 +41,31 @@ char debugBuffer[255];
 #define LED_TIME 500
 #define LED_PIN 9
 
+#define SPEEDUINO_FETCH_INTERVAL 1000
+#define CANBUS_FETCH_INTERVAL 2000
+#define CANBUS_SEND201_INTERVAL 3000
+#define CANBUS_SEND420_INTERVAL 6000
+
 byte dataCoolant; // current coolant
 word dataRpm;     // current rpm
 word dataSpeed;   // current speed
 
-word requiredBytes = 0; // required number of bytes
-word receivedBytes = 0; // received number of bytes
-byte serialBuffer[255];  // buffer for storing serial data
+unsigned long currentMillis;
 
-unsigned long lastMillis;
+// the last time the action was successful
+unsigned long speeduinoFetchLastMillis;
+unsigned long canbusFetchLastMillis;
+unsigned long canbusSend201LastMillis;
+unsigned long canbusSend420LastMillis;
 
-enum State {
-  state_waiting,
-  state_request_data,
-  state_reading_data,
-  state_storing_data,
-  state_reading_canbus,
-  state_writing_canbus,
-  state_error
-};
+// true if request sent to speeduino
+// false if awaiting response
+bool speeduinoRequested = false;
 
 #ifdef DEBUG
 HardwareSerial &debugger = Serial;
 #endif
+
 #ifdef USE_SW_SERIAL
 SoftwareSerial speeduino(SW_SERIAL_RX, SW_SERIAL_TX);
 #endif
@@ -76,21 +77,24 @@ MCP2515 mcp2515(MCP2515_CS);
 struct can_frame canMsg201;
 struct can_frame canMsg420;
 struct can_frame canMsg4B0;
-
-State currentState = state_waiting;
+bool shownNoMessageError = false; // flag so we only show NO MESSAGE error once
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  
   #ifdef DEBUG
-  debugger.begin(SERIAL_BAUD);
+  debugger.begin(DEBUG_BAUD);
   #endif
+  
   speeduino.begin(SPEEDUINO_BAUD);
+  
   mcp2515.reset();
   mcp2515.setBitrate(MCP2515_BITRATE, MCP2515_CLOCK);
   mcp2515.setNormalMode();
+  
   #ifdef DEBUG
   debugger.println("speeduino_can_bridge");
-  sprintf(debugBuffer, "debug on serial @ %li", SERIAL_BAUD);
+  sprintf(debugBuffer, "debug on serial @ %li", DEBUG_BAUD);
   debugger.println(debugBuffer);
   #ifdef USE_SW_SERIAL
   sprintf(debugBuffer, "speeduino on sw serial @ %li ", SPEEDUINO_BAUD);
@@ -100,152 +104,135 @@ void setup() {
   #endif
   debugger.println(debugBuffer);
   #endif
-  lastMillis = millis();
+
+  speeduinoFetchLastMillis = 0;
+  canbusFetchLastMillis = 0;
+  canbusSend201LastMillis = 0;
+  canbusSend420LastMillis = 0;
 }
 
 void loop() {
+  currentMillis = millis();
+
+  // flash the LED on LED_PIN once per LED_TIME
   digitalWrite(LED_PIN, (millis() % LED_TIME) > LED_TIME / 2);
-  switch (currentState) {
-    case state_waiting:
-      if (timeout()) {
-        #ifdef DEBUG
-        debugger.println("timeout elapsed");
-        #endif
-        currentState = state_request_data;
+
+  if (currentMillis - speeduinoFetchLastMillis >= SPEEDUINO_FETCH_INTERVAL) {
+    if (!speeduinoRequested) {
+      speeduino_request();
+    } else {
+      if (speeduino_read()) {
+        speeduinoFetchLastMillis = currentMillis;
       }
-      break;
-    case state_request_data:
-      #ifdef DEBUG
-      debugger.println("requesting data");
-      #endif
-      speeduinoRequestRealtime(0, DATA_TO_REQUEST);
-      currentState = state_reading_data;
-      #ifdef DEBUG
-      debugger.println("reading serial");
-      #endif
-      break;
-    case state_reading_data:
-      if (readSerial()) {
-        #ifdef DEBUG
-        debugger.println("finished reading serial");
-        #endif
-        currentState = state_storing_data;
-      }
-      break;
-    case state_storing_data:
-      // first byte should be r and second should be command
-      if (serialBuffer[0] == (byte)'r' && serialBuffer[1] == SPEEDUINO_R_COMMAND) {
-        #ifdef DEBUG
-        debugger.println("storing data");
-        #endif
-        dataCoolant = serialBuffer[2 + COOLANT_OFFSET];
-        dataRpm = word(serialBuffer[2 + RPM_OFFSET + 1], serialBuffer[2 + RPM_OFFSET + 0]);
-        currentState = state_reading_canbus;
-        #ifdef DEBUG
-        debugger.println("waiting canbus message 0x4B0");
-        #endif
-      } else {
-        #ifdef DEBUG
-        sprintf(debugBuffer, "error 0x%02X 0x%02X", serialBuffer[0], serialBuffer[1]);
-        debugger.println(debugBuffer);
-        #endif
-        currentState = state_error;
-      }
-      break;
-    case state_reading_canbus:
-      {
-        MCP2515::ERROR e = mcp2515.readMessage(&canMsg4B0);
-        if (e == MCP2515::ERROR_OK) {
-          #ifdef DEBUG
-          sprintf(debugBuffer, "got can bus frame with id: 0x%02X", (int)canMsg4B0.can_id);
-          debugger.println(debugBuffer);
-          #endif
-          if (canMsg4B0.can_id == 0x4B0) {
-            #ifdef DEBUG
-            debugger.println("reading canbus message 0x4B0");
-            #endif
-            dataSpeed = word(canMsg4B0.data[4], canMsg4B0.data[5]);
-            currentState = state_writing_canbus;
-          }
-        } else {
-          #ifdef DEBUG
-          if (e != MCP2515::ERROR_NOMSG) {
-            sprintf(debugBuffer, "can bus error: %d", e);
-            debugger.println(debugBuffer);
-          }
-          #endif
-        }
-      }
-      break;
-    case state_writing_canbus:
-      #ifdef DEBUG
-      debugger.println("writing to canbus");
-      #endif
-      stateWritingCanbus();
-      currentState = state_waiting;
-      break;
-    case state_error:
-      // do nothing
-      break;
-    default:
-      // shouldn't get here...
-      break;
+    }
+  }
+
+  if (currentMillis - canbusFetchLastMillis >= CANBUS_FETCH_INTERVAL) {
+    if (canbus_fetch()) {
+      canbusFetchLastMillis = currentMillis;
+    }
+  }
+
+  if (currentMillis - canbusSend201LastMillis >= CANBUS_SEND201_INTERVAL) {
+    if (canbus_send_201()) {
+      canbusSend201LastMillis = currentMillis;
+    }
+  }
+
+  if (currentMillis - canbusSend420LastMillis >= CANBUS_SEND420_INTERVAL) {
+    if (canbus_send_420()) {
+      canbusSend420LastMillis = currentMillis;
+    }
   }
 }
 
-// check how many millis have elapsed and return true if we
-// have reached the required number
-bool timeout() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - lastMillis >= MILLIS_BETWEEN_READS) {
-    lastMillis = currentMillis;
+void speeduino_request() {
+  #ifdef DEBUG
+  sprintf(debugBuffer, "sending serial request 0x%02X 0x%02X", SPEEDUINO_COMMAND, SPEEDUINO_COMMAND_TYPE);
+  debugger.println(debugBuffer);
+  #endif
+
+  speeduino.write((byte)SPEEDUINO_COMMAND);
+  speeduino.write((byte)SPEEDUINO_CANID);
+  speeduino.write((byte)SPEEDUINO_COMMAND_TYPE);
+  speeduino.write((byte)0x00);
+  speeduino.write((byte)0x00); // offset 0x0000
+  speeduino.write((byte)lowByte(DATA_TO_REQUEST));
+  speeduino.write((byte)highByte(DATA_TO_REQUEST));
+
+  speeduinoRequested = true;
+}
+
+bool speeduino_read() {
+  if (speeduino.available() >= DATA_TO_REQUEST + 2) {
+    char cmd = speeduino.read();
+    char cmd_type = speeduino.read();
+
+    #ifdef DEBUG
+    sprintf(debugBuffer, "received serial response 0x%02X 0x%02X", cmd, cmd_type);
+    debugger.println(debugBuffer);
+    #endif
+    
+    if (cmd == SPEEDUINO_COMMAND && cmd_type == SPEEDUINO_COMMAND_TYPE) {
+      for (int i = 0; i < DATA_TO_REQUEST; i++) {
+        byte data = speeduino.read();
+        switch (i) {
+          case COOLANT_OFFSET:
+            dataCoolant = data;
+            break;
+          case RPM_OFFSET_LO:
+            dataRpm = word(dataRpm, data);
+            break;
+          case RPM_OFFSET_HI:
+            dataRpm = word(data, dataRpm);
+            break;
+        }
+      }
+    } else {
+      // error, empty the buffer
+      while(speeduino.available()) {
+        speeduino.read();
+      }
+    }
+    speeduinoRequested = false;
     return true;
   }
   return false;
 }
 
-// send a request for realtime data
-// reset received bytes and set required data length
-void speeduinoRequestRealtime(word data_offset, word data_length) {
-  speeduino.write((byte)'r');
-  speeduino.write((byte)SPEEDUINO_CANID);
-  speeduino.write((byte)SPEEDUINO_R_COMMAND);
-  speeduino.write((byte)lowByte(data_offset));
-  speeduino.write((byte)highByte(data_offset));
-  speeduino.write((byte)lowByte(data_length));
-  speeduino.write((byte)highByte(data_length));
-
-  receivedBytes = 0;
-  requiredBytes = data_length + 2;
-}
-
-// read from serial, return true if read enough bytes into buffer
-bool readSerial() {
-  while (speeduino.available() && receivedBytes < requiredBytes) {
-    serialBuffer[receivedBytes] = speeduino.read();
+bool canbus_fetch() {
+  MCP2515::ERROR e = mcp2515.readMessage(&canMsg4B0);
+  if (e == MCP2515::ERROR_OK) {
+    shownNoMessageError = false;
+    
     #ifdef DEBUG
-    sprintf(debugBuffer, "0x%02X", serialBuffer[receivedBytes]);
+    sprintf(debugBuffer, "got can bus frame with id: 0x%02X", (int)canMsg4B0.can_id);
     debugger.println(debugBuffer);
     #endif
-    receivedBytes++;
+
+    if (canMsg4B0.can_id == 0x4B0) {
+      #ifdef DEBUG
+      debugger.println("reading canbus message 0x4B0");
+      #endif
+
+      dataSpeed = word(canMsg4B0.data[4], canMsg4B0.data[5]);
+      return true;
+    }
+  } else {
+    #ifdef DEBUG
+    if (e != MCP2515::ERROR_NOMSG || !shownNoMessageError) {
+      shownNoMessageError = true;
+      sprintf(debugBuffer, "can bus error: %d", e);
+      debugger.println(debugBuffer);
+    }
+    #endif
   }
-  return receivedBytes == requiredBytes;
+  return false;
 }
 
-void stateWritingCanbus() {
-  word adjustedRpm = dataRpm * 4;
-
-  byte adjustedCoolant;
-  
-  if (dataCoolant<=60) adjustedCoolant=0x28;
-  else if (dataCoolant<=74) adjustedCoolant=0x63;
-  else if (dataCoolant<=90) adjustedCoolant=0x88;
-  else if (dataCoolant<=99) adjustedCoolant=0x9C;
-  else adjustedCoolant=0xD0;
-
+bool canbus_send_201() {
   #ifdef DEBUG
-  sprintf(debugBuffer, "coolant: %d", dataCoolant);
-  debugger.println(debugBuffer);
+  debugger.println("sending 201");
   
   sprintf(debugBuffer, "rpm: %d", dataRpm);
   debugger.println(debugBuffer);
@@ -253,7 +240,9 @@ void stateWritingCanbus() {
   sprintf(debugBuffer, "speed: %d (%dmph)", dataSpeed, (int)((dataSpeed * 0.0066)-66));
   debugger.println(debugBuffer);
   #endif
-
+  
+  word adjustedRpm = dataRpm * 4;
+  
   canMsg201.can_id = 0x201;
   canMsg201.can_dlc = 8;
   canMsg201.data[0] = highByte(adjustedRpm);// rpm
@@ -264,17 +253,50 @@ void stateWritingCanbus() {
   canMsg201.data[5] = lowByte(dataSpeed);   // speed
   canMsg201.data[6] = 0x00;                 // 
   canMsg201.data[7] = 0x00;                 // 
-  mcp2515.sendMessage(&canMsg201);
+  
+  MCP2515::ERROR e = mcp2515.sendMessage(&canMsg201);
 
+  #ifdef DEBUG
+  sprintf(debugBuffer, "can bus error: %d", e);
+  debugger.println(debugBuffer);
+  #endif
+  
+  return e == MCP2515::ERROR_OK;
+}
+
+bool canbus_send_420() {
+  debugger.println("sending 420");
+
+  #ifdef DEBUG
+  sprintf(debugBuffer, "coolant: %d", dataCoolant);
+  debugger.println(debugBuffer);
+  #endif
+  
+  byte adjustedCoolant;
+  
+  if (dataCoolant<=60) adjustedCoolant=0x28;
+  else if (dataCoolant<=74) adjustedCoolant=0x63;
+  else if (dataCoolant<=90) adjustedCoolant=0x88;
+  else if (dataCoolant<=99) adjustedCoolant=0x9C;
+  else adjustedCoolant=0xD0;
+  
   canMsg420.can_id = 0x420;
   canMsg420.can_dlc = 8;
-  canMsg420.data[0] = adjustedCoolant;  // ect
-  canMsg420.data[1] = 0x00;         // pres.
-  canMsg420.data[2] = 0x00;         // fuel flow
-  canMsg420.data[3] = 0x00;         // prndl
-  canMsg420.data[4] = 0x00;         // MIL/overdrive
-  canMsg420.data[5] = 0x00;         // safe cooling/PATS
-  canMsg420.data[6] = 0x00;         // charging system status
-  canMsg420.data[7] = 0x00;         // engine off elapsed time
-  mcp2515.sendMessage(&canMsg420);
+  canMsg420.data[0] = adjustedCoolant;      // ect
+  canMsg420.data[1] = 0x00;                 // pres.
+  canMsg420.data[2] = 0x00;                 // fuel flow
+  canMsg420.data[3] = 0x00;                 // prndl
+  canMsg420.data[4] = 0x00;                 // MIL/overdrive
+  canMsg420.data[5] = 0x00;                 // safe cooling/PATS
+  canMsg420.data[6] = 0x00;                 // charging system status
+  canMsg420.data[7] = 0x00;                 // engine off elapsed time
+  
+  MCP2515::ERROR e = mcp2515.sendMessage(&canMsg420);
+  
+  #ifdef DEBUG
+  sprintf(debugBuffer, "can bus error: %d", e);
+  debugger.println(debugBuffer);
+  #endif
+  
+  return e == MCP2515::ERROR_OK;
 }
